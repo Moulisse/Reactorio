53703556e2fc3e8a3302ee00f1e6da62c4e4feb1
denis <denis.lachartre@x-gil.fr>
2022-07-06 19:44:47 +0200

    -trx

--- src/assets/town.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.0" name="town" tilewidth="8" tileheight="8" tilecount="168" columns="8">
- <image source="galletcity/galletcity_tiles.png" width="64" height="168"/>
-</tileset>
